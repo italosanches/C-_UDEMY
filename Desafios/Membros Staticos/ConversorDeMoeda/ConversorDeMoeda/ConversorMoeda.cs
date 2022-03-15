c01017bd392ed47747f7ee61e84e8869b45cff01
italosanches <italo_carvalho157@hotmail.com>
2022-03-14 21:05:12 -0300

    Adição do exercicio com classe abstrata / correção de pasta dos desafios

---
+++ Desafios/Membros Staticos/ConversorDeMoeda/ConversorDeMoeda/ConversorMoeda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeMoeda
+{
+    internal class ConversorMoeda
+    {
+        public static double IOF = 0.06;
+        public static double ValorReal;
+        public static double ValorJuros;
+
+        public static double Valor (double cotacao, double quantidade)
+        {
+            ValorJuros = (cotacao * quantidade) * IOF; 
+            ValorReal = (cotacao * quantidade)+ ValorJuros;
+            return ValorReal;
+        }
+
+
+
+    }
+}
---
+++ Desafios/Membros Staticos/ConversorDeMoeda/ConversorDeMoeda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace ConversorDeMoeda
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            Console.Write("Qual é a cotação do dolar?: ");
+            double dolar =double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quanto dolares você vai comprar? ");
+            double quantidade = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine($"Valor a ser pago em reais {ConversorMoeda.Valor(dolar, quantidade).ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
