423a7ba4362ef49f90dfe1acd34497c0de3ec02b
italosanches <italo_carvalho157@hotmail.com>
2022-03-16 22:55:14 -0300

    Finalização da solução do desafio Conta Corrente

---
+++ Desafios/Conta Corrente/ContaCorrente/ContaCorrente/Conta.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ContaCorrente
+{
+    internal class Conta
+    {
+        private string _titular;
+        public int ContaCorrente { get; private set; }
+        public double Saldo { get; private set; }
+
+
+        public Conta(int conta, string titular)
+        {
+            ContaCorrente = conta;
+            _titular = titular;
+            Saldo = 0;
+        }
+
+        public string Titular
+        {
+            get { return _titular; }
+            set
+            {
+                if (value != null && value.Length >1)
+                {
+                    _titular = value;
+                }
+            }
+        }
+
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque (double saque)
+        {
+             Saldo =Saldo - saque - 5;
+            
+        }
+
+        public bool VerificaSaldo ()
+        {
+            if (Saldo >= 0)
+            {
+                return true;
+            }
+            else
+            {
+               return false;
+            }
+        }
+         
+
+        public override string ToString()
+        {
+            return "Conta: " + ContaCorrente + "  Titular: "+ _titular + "  Saldo: $ "+Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+
+
+    }
+}
---
+++ Desafios/Conta Corrente/ContaCorrente/ContaCorrente/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace ContaCorrente
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            
+
+            Console.Write("Digite o numero da conta corrente: ");
+            int conta = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Digite o nome do titular da conta corrente :");
+            string titular =Console.ReadLine();
+
+            Conta contacorrente = new Conta(conta, titular);
+
+            Console.WriteLine("Havera deposito inicial na conta? S/N");
+            string opcao =Console.ReadLine().ToUpper();
+
+            if(opcao == "S")
+            {
+                Console.Write("Digite o valor do deposito: ");
+                double deposito = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                contacorrente.Deposito(deposito);
+            }
+            Console.WriteLine(contacorrente);
+            
+
+            Console.Write("Entre um valor para deposito: ");
+            double primeiroDeposito = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            contacorrente.Deposito(primeiroDeposito);
+            Console.WriteLine("Dados da conta atualizado:");
+            Console.WriteLine(contacorrente);
+
+            Console.WriteLine("Digite o valor para saque ");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contacorrente.Saque(valorSaque);
+            
+
+            if(contacorrente.VerificaSaldo())
+            {
+                Console.WriteLine("Saque autorizado, dados da conta atualizado: ");
+                Console.WriteLine(contacorrente);
+            }
+            else
+            {
+                Console.WriteLine("Saque bloqueado o seu saldo ficaria negativo. Saldo: " + contacorrente.Saldo);
+            }
+        }
+}
+}
