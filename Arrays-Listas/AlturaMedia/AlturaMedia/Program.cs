03e2675de2191008c9e1ac0ad78eb6e908a09599
italosanches <italo_carvalho157@hotmail.com>
2022-03-21 20:33:34 -0300

    Criação de exercicio com vetores e classes.

---
+++ Arrays-Listas/AlturaMedia/AlturaMedia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+namespace AlturaMedia 
+{ 
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Digite a quantidade de pessoas para o calculo.");
+            int pessoas = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double [] vetor = new double[pessoas];
+            for (int i = 0; i < pessoas; i++)
+            {
+                vetor[i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            }
+            double soma = 0.00;
+
+            for (int i = 0; i < pessoas; i++)
+            {
+                soma += vetor[i];
+            }
+            double media = soma / pessoas;
+             Console.WriteLine("Media = " + media.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ Arrays-Listas/MediaProduto/MediaProduto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+namespace MediaProduto
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Digite a quantidade de produtos");
+            int n = int.Parse(Console.ReadLine());
+            Product[] produtos = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite o nome do produto: ");
+                string nome = Console.ReadLine();
+                Console.Write("Digite o valor do produto: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) ;
+
+                produtos[i] = new Product(nome, price);
+            }
+            double soma = 0.00;
+            for (int i = 0; i < n; i++)
+            {
+                 soma += produtos[i].Price;
+            }
+            double media = soma / n;
+            Console.WriteLine(media.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
