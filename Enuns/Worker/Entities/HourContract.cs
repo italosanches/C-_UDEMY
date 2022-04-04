bf2d3d721b9d48108835780be3256aee82b8130b
italosanches <italo_carvalho157@hotmail.com>
2022-04-04 20:26:16 -0300

    Criação de classes e Enuns exercicio Resolvido Worker

---
+++ Enuns/Worker/Entities/HourContract.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker.Entities
+{
+     class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuerPerHour { get; set; }
+
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+
+        }
+        public HourContract(DateTime date,double valuePerHour,int hours)
+        {
+            Date = date;
+            ValuerPerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return ValuerPerHour * Hours;
+        }
+    }
+}
