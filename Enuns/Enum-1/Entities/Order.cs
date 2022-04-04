dbef2f2e3b64f39c95631b11a248f12ecdb5908f
italosanches <italo_carvalho157@hotmail.com>
2022-04-04 20:25:32 -0300

    Enum basico

---
+++ Enuns/Enum-1/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using Enum_1.Entities.Enuns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_1.Entities
+{
+     class Order
+    {
+        public int ID { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return ID + ", " + Moment + ", " + Status;
+        }
+    }
+}
---
+++ Enuns/Enum-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Enum_1.Entities;
+using Enum_1.Entities.Enuns;
+
+namespace Enum_1
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Order order = new Order() { ID = 100,
+            Moment = DateTime.Now,
+            Status = OrderStatus.Delivered};
+
+            Console.WriteLine(order.ToString());
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+
+        
+    }
+}
